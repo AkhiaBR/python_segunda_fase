8f3391791a6269921894ede66e7048e49f5cc09c
Fernando Gonçalves <akhiatoshi64@gmail.com>
2024-08-11 23:18:32 -0300

    Exercícios de Tuplas

---
+++ exercicio05.py
@@ -0,0 +1,12 @@
+# Exercício 05
+# Nome: Fernando Gonaçalves
+
+produtos_mercado = (
+    ("Café Espresso",12.99)
+    ("Queijo Brie",25.49)
+    ("Açúcar Orgânico",8.79) 
+    ("Pão Integral",4.29)
+    ("Manteiga de Amendoim",19.99) 
+    ("Uva Passa",6.99)
+)
+
