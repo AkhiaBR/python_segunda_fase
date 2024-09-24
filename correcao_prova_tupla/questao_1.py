5e29aa88aa91cb593dd78fb76e6eb9fbb21de669
Fernando <fernandooogoncalves@gmail.com>
2024-09-24 13:56:45 -0300

    first commit

---
+++ correcao_prova_tupla/questao_1.py
@@ -0,0 +1,8 @@
+# questão 1
+
+tupla_times = (
+    "botafogo", "Fortaleza", "Flamengo", "Palmeiras", "São Paulo", "Cruzeiro", "Bahia", "Athletico-PR", "Atlético-MG", "Vasco", "Bragantino",
+    "Juventude", "Grêmio", "Criciúma", "Internacional", "Vitória", "Corinthians", "Fluminense", "Cuiabá", "Atlético-GO"
+)
+
+primeiro_cinco = tupla_times.index
