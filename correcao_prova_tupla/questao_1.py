61b06a8fb608b4ba422fde54db14e366b45f2176
Fernando <fernandooogoncalves@gmail.com>
2024-09-24 15:11:30 -0300

    add: questa_2.py

--- correcao_prova_tupla/questao_1.py
+++ correcao_prova_tupla/questao_1.py
@@ -1,8 +1,15 @@
 # questão 1
 
 tupla_times = (
-    "botafogo", "Fortaleza", "Flamengo", "Palmeiras", "São Paulo", "Cruzeiro", "Bahia", "Athletico-PR", "Atlético-MG", "Vasco", "Bragantino",
+    "Botafogo", "Fortaleza", "Flamengo", "Palmeiras", "São Paulo", "Cruzeiro", "Bahia", "Athletico-PR", "Atlético-MG", "Vasco", "Bragantino",
     "Juventude", "Grêmio", "Criciúma", "Internacional", "Vitória", "Corinthians", "Fluminense", "Cuiabá", "Atlético-GO"
 )
 
-primeiro_cinco = tupla_times.index
+primeiro_cinco = tupla_times[:5]
+ultimo_quatro = tupla_times[-4:]
+ordem_alfa = sorted(tupla_times)
+posicao_corin = tupla_times.index("Corinthians")+1
+print(f"Primeiros cinco colocados: {primeiro_cinco}")
+print(f"Últimos quatro colocados: {ultimo_quatro}")
+print(f"Tupla em ordem alfabética: {ordem_alfa}")
+print(f"Qual a posição do Corinthians na lista: {posicao_corin}")
---
+++ correcao_prova_tupla/questao_2.py
@@ -0,0 +1,51 @@
+# questão 2
+
+funcionarios = {}
+
+def cadastrar():
+    funcionario_lista = []
+
+    nome = str(input("Digite o nome do funcionário: "))
+    cargo = str(input("Digite o cargo desse funcionário: "))
+
+    funcionario_lista.append(nome)
+    funcionario_lista.append(cargo)
+    funcionarios[len(funcionarios)+1] = funcionario_lista
+def alterar():
+    cod_funcionario = int(input("Qual o código do funcionário que deseja alterar?: "))
+    op = str(input("O que você deseja alterar? Nome (N) ou cargo (C): ")).upper().strip()
+    if(op=="N"):
+        nome = str(input("Digite o novo nome do funcionário: "))
+        for i in funcionarios[cod_funcionario]:
+            if(i==nome):
+                print("ERRO: Valor já inserido. Tente novamente...")
+        # else:
+        #     funcionarios[cod_funcionario] = 
+            
+        
+# def excluir():
+def pesquisar():
+    print(funcionarios)
+
+while True:
+    print("___________________________")
+    print("C - Cadastrar")
+    print("A - Alterar")
+    print("E - Excluir")
+    print("P - Pesquisar")
+    print("S - Sair")
+    print("___________________________")
+    op = str(input("Digite a letra referente à opção desejada no menu superior: ")).upper().strip()
+
+    if(op=="C"):
+        cadastrar()
+    elif(op=="A"):
+        alterar()
+    # elif(op=="E"):
+    #     excluir()
+    elif(op=="P"):
+        pesquisar()
+    elif(op=="S"):
+        break
+    else:
+        print("ERRO: Valor inserido errado. Digite novamente um novo valor...")
