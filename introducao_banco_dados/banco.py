6c2761beb280410e796d1e782ec1732fd8178396
Fernando <fernandooogoncalves@gmail.com>
2024-10-01 17:24:24 -0300

    update: banco.py

--- introducao_banco_dados/banco.py
+++ introducao_banco_dados/banco.py
@@ -1,13 +1,13 @@
-import mysql.connector # importa AS MINHAS BOLAS
+import mysql.connector # importa a função de import do conector mysql
 
-conexao_banco = mysql.connector.connect( # especifica AS MINHAS BOLAS
+conexao_banco = mysql.connector.connect( # especifica a conexão com o database
     host = "127.0.0.1",
     user = "root",
     password = "",
     database = "vendas"
 )
 
-cursor = conexao_banco.cursor() # define MINHAS BOLAS
+cursor = conexao_banco.cursor() # define que conexao_banco é um objeto manipulável por comandos SQL
 
 # CRUD (CREATE, READ, UPDATE, DELETE)
 
@@ -16,11 +16,11 @@ cursor = conexao_banco.cursor() # define MINHAS BOLAS
 nome = str(input("Digite o nome do produto: "))
 valor = float(input("Digite o valor do produto: "))
 
-comando_sql = (f'INSERT INTO produtos (nome_produto,valor_produto) VALUES ("{nome}",{valor})') # define MINHAS BOLAS
-cursor.execute(comando_sql) # executa MINHAS BOLAS
-conexao_banco.commit() # commita MINHAS BOLAS
+comando_sql = (f'INSERT INTO produtos (nome_produto,valor_produto) VALUES ("{nome}",{valor})') # define o que vai ser colocado na tabela
+cursor.execute(comando_sql) # executa as alterações, executando a variável cursor criada anteriormente
+conexao_banco.commit() # commita as alterações no banco
 
-# UPDATE NA MINHAS BOLAS
+# UPDATE
 
 id_p = int(input("Digite o ID do produto que deseja alterar: "))
 nome = str(input("Digite o novo nome do produto: "))
